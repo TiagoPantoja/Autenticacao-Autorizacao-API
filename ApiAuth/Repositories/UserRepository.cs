2c0844a9f6ce6de28146933e87810a19539a44ab
Tiago Luis Souza Pantoja <tiagopantoja1998@gmail.com>
2023-08-03 14:49:57 -0300

    Adicionando arquivos principais do projeto

---
+++ ApiAuth/Repositories/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography.Xml;
+using ApiAuth.Models;
+
+namespace ApiAuth.Repositories;
+
+public class UserRepository
+{
+    public static User Get(string username, string password)
+    {
+        var users = new List<User>
+        {
+            new() { Id = 1, Username = "Batman", Password = "batman", Role = "manager" },
+            new() { Id = 2, Username = "Robin", Password = "robin", Role = "employee" }
+        };
+        return users
+            .FirstOrDefault(x =>
+                x.Username == username
+                && x.Password == password);
+    }
+}
